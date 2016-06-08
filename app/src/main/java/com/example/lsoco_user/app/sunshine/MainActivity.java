2001d87321bd3688b01d75a988860460da0c1068
Liviu Victor Socolovici <lsocolovici1979@gmail.com>
2016-06-08 14:25:18 -0400

    Initial commit - fetches (and logs) weather data on Menu->Refresh - displays mock data

---
+++ app/src/main/java/com/example/lsoco_user/app/sunshine/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.lsoco_user.app.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                Log.d(LOG_TAG, "selected " + item.getTitle());
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+}
