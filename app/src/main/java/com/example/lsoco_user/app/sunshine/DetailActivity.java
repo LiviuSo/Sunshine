226fe077cd6a17cb46b4cb5066564d92d7c991cf
Liviu Victor Socolovici <lsocolovici1979@gmail.com>
2016-07-09 15:28:11 -0400

    added starting code for Lesson 4

--- app/src/main/java/com/example/lsoco_user/app/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/lsoco_user/app/sunshine/DetailActivity.java
@@ -1,11 +1,17 @@
 package com.example.lsoco_user.app.sunshine;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +25,12 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
 
-
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -44,17 +50,23 @@ public class DetailActivity extends AppCompatActivity {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_share) {
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        private final String LOG_TAG                = DetailFragment.class.getSimpleName();
+        private final String FORECAST_SHARE_HASHTAG = " #unshineApp";
+        private String mForecast;
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -65,9 +77,30 @@ public class DetailActivity extends AppCompatActivity {
             TextView textView = (TextView) rootView.findViewById(R.id.text_details);
             Intent intent = getActivity().getIntent();
             if (intent != null) {
-                textView.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+                mForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                textView.setText(mForecast);
             }
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment, menu);
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+            ShareActionProvider mShareProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            if(mShareProvider != null) {
+                mShareProvider.setShareIntent(createForecastShareIntent());
+            } else {
+                Log.e(LOG_TAG, "ShareActionProvider is null");
+            }
+        }
+
+        private Intent createForecastShareIntent() {
+            Intent sendIntent = new Intent(Intent.ACTION_SEND);
+            sendIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            sendIntent.setType("text/plain");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, mForecast + FORECAST_SHARE_HASHTAG);
+            return sendIntent;
         }
     }
 }
